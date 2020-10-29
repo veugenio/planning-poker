418ceb5b288d1e6ffba408f293c49b2adeef2dd7
Vicente Eugenio <veugenio@its.jnj.com>
2020-10-29 01:04:38 -0300

    Players broadcasting

--- server/server.js
+++ server/server.js
@@ -12,27 +12,50 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
-let interval;
-
 let clients = [];
+let players = [];
 
 io.on("connection", (socket) => {
   console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-console.log(socket)
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
+  console.log(socket)
+
+  // Connections pool.
+  clients.push(socket);
+
+  registerNewPlayer(socket);
+
+
+  socket.on("disconnect", (data) => {
     console.log("Client disconnected");
-    // clearInterval(interval);
+    console.log(data)
+
+    // Update clients id.
+    clients = clients.filter(client => client.id !== socket.id);
+    // Remove player
+    removePlayer(socket);
   });
 });
 
-const getApiAndEmit = socket => {
-  const response = {players: []};
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
+const removePlayer = socket => {
+  players = players.filter(player => player.id !== socket.id);
+  broadcastPlayers(socket);
+}
+
+const registerNewPlayer = socket => {
+
+  players.push({
+    name: 'Anonymous',
+    id: socket.id
+  });
+
+  broadcastPlayers(socket);
+}
+
+const broadcastPlayers = socket => {
+  const { id } = socket;
+  clients.filter(client => client.id !== id).forEach(client => {
+    client.emit("FromAPI", { players: players });
+  })
+}
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
