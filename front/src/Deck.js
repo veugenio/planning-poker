916309a895d98697d8fb9a66e3ac37db39746f78
Vicente Eugenio <veugenio@its.jnj.com>
2020-10-28 23:42:03 -0300

    Using the state hook

--- front/src/Deck.js
+++ front/src/Deck.js
@@ -1,45 +1,31 @@
 import './Deck.css';
 import Card from './Card.js';
-import React from 'react';
+import { useState } from 'react';
 
-class Deck extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cards: [
-        { value: '0', active: 0 },
-        { value: '1', active: 0 },
-        { value: '2', active: 0 },
-        { value: '3', active: 0 },
-        { value: '5', active: 0 },
-        { value: '8', active: 0 },
-        { value: '13', active: 0 },
-        { value: '?', active: 0 }
-      ]
-    };
- 
-    this.select = this.select.bind(this);
-  }
-
-  select (value) {
-    const cards = this.state.cards.map(card => {
+function Deck () {
+  const [ cards, setCards ] = useState([
+    { value: '0', active: 0 },
+    { value: '1', active: 0 },
+    { value: '2', active: 0 },
+    { value: '3', active: 0 },
+    { value: '5', active: 0 },
+    { value: '8', active: 0 },
+    { value: '13', active: 0 },
+    { value: '?', active: 0 }
+  ]);
+  const select = value => {
+    setCards(cards.map(card => {
       card.active = card.value === value;
       return card;
-    });
-    this.setState({cards: cards});
-  }
-
-  render () {
-    return (
-      <div className="Deck">
-        {this.state.cards.map(card =>
-          <Card key={card.value} value={card.value} active={card.active} onActivate={this.select} />
-        )}
-      </div>
-    );
-  }
+    }));
+  };
+  return (
+    <div className="Deck">
+      {cards.map(card =>
+        <Card key={card.value} value={card.value} active={card.active} onActivate={select} />
+      )}
+    </div>
+  );
 }
 
 export default Deck;
